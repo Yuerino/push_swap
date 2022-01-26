88ffe69634f5188391dc4706632d4c561a6bd204
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-01-26 22:17:04 +0100

    add header

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 22:01:05 by cthien-h          #+#    #+#             */
+/*   Updated: 2022/01/26 22:10:31 by cthien-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+
+# include "libft.h"
+
+typedef struct s_stack
+{
+	t_list	*stack;
+	int		length;
+}	t_stack;
+
+#endif
